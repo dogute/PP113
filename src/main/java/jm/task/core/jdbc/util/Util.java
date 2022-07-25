eadda5af9e30bced50feed79c85f507501e81648
dogute <dogute7@gmail.com>
2022-07-25 17:03:57 +1000

    113

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,27 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/justfortest";
+    private static final String USERNAME = "root1";
+    private static final String PASSWORD = "root";
+
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
+
+}
+
